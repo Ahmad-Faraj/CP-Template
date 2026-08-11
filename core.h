1a6203edd654586c9f8efad1122f6d6abf48c5cc
Ahmed Salama <ahsalamase@gmail.com>
2026-08-12 01:53:09 +0300

    Update .gitignore and core.h: add object file exclusion and enhance mathematical macros

--- core.h
+++ core.h
@@ -9,3 +9,11 @@ const int N = 3e5 + 9;
 const int mod = 1e9 + 7;
 const int inf = 2e9;
 const ll INF = 2e18;
+
+#define sz(x) (int)(x.size())
+const double PI = acos(-1);
+const double EPS = 1e-9;
+
+#define sub_mod(a, b, m) ((((a) % (m)) - ((b) % (m)) + (m)) % (m))
+#define add_mod(a, b, m) ((((a) % (m)) + ((b) % (m))) % (m))
+#define mult_mod(a, b, m) ((((a) % (m)) * ((b) % (m))) % (m))
