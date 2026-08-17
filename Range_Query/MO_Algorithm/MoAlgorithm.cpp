3260f6b5d559427ce7b88d294f6f4758228ca633
Ahmed Salama <ahsalamase@gmail.com>
2026-08-17 17:05:05 +0300

    Change at MO templete

--- Range_Query/MO_Algorithm/MoAlgorithm.cpp
+++ Range_Query/MO_Algorithm/MoAlgorithm.cpp
@@ -19,64 +19,49 @@ const int N = 200000 + 5;
 const int SQ = 450;
 const int HILBERT_POW = 21;
 
-// static inline int64_t hilbertOrder(int x, int y, int pow, int rotate)
-// {
-//     if (pow == 0)
-//         return 0;
-//     int hpow = 1 << (pow - 1);
-//     int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
-//     seg = (seg + rotate) & 3;
-//     const int rotateDelta[4] = {3, 0, 0, 1};
-//     int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
-//     int nrot = (rotate + rotateDelta[seg]) & 3;
-//     int64_t subSquareSize = int64_t(1) << (2 * pow - 2);
-//     int64_t ordd = seg * subSquareSize;
-//     int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
-//     ordd += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-//     return ordd;
-// }
+static inline int64_t hilbertOrder(int x, int y, int pow, int rotate)
+{
+    if (pow == 0)
+        return 0;
+    int hpow = 1 << (pow - 1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rotate) & 3;
+    const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    int64_t subSquareSize = int64_t(1) << (2 * pow - 2);
+    int64_t ordd = seg * subSquareSize;
+    int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
+    ordd += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+    return ordd;
+}
 
-// struct Query
-// {
-//     int l, r, q_idx, blk_idx;
-//     int64_t ord;
-
-//     Query() {}
-
-//     Query(int l, int r, int q_idx)
-//     {
-//         this->l = l;
-//         this->r = r;
-//         this->q_idx = q_idx;
-//         blk_idx = l / SQ;
-
-//         ord = hilbertOrder(l - 1, r - 1, HILBERT_POW, 0);
-//     }
-
-//     bool operator<(const Query &other) const
-//     {
-//         return ord < other.ord;
-//     }
-// };
-
-struct Query {
+struct Query
+{
     int l, r, q_idx, blk_idx;
+    // int64_t ord;
 
-    Query() {
-    }
+    Query() {}
 
-    Query(int l, int r, int q_idx) {
+    Query(int l, int r, int q_idx)
+    {
         this->l = l;
         this->r = r;
         this->q_idx = q_idx;
         blk_idx = l / SQ;
+
+        // ord = hilbertOrder(l - 1, r - 1, HILBERT_POW, 0);
     }
 
-    bool operator<(const Query &other) const {
+    bool operator<(const Query &other) const
+    {
         if (blk_idx != other.blk_idx) return blk_idx < other.blk_idx;
         return r < other.r;
+        
+        // return ord < other.ord;
     }
 };
+
 
 ll n, q, arr[N], ans[200005];
 Query query[200005];
